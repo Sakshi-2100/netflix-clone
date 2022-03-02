a9ba87991ec0e3b2999b2036d2b180db0093c090
Sakshi Soni <65129236+Sakshi-2100@users.noreply.github.com>
2022-03-02 20:28:37 +0530

    Updated Row.js and added css to Row component

--- src/Row.js
+++ src/Row.js
@@ -1,20 +1,37 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 import axios from "./axios";
+import "./Row.css";
 
+const base_url = `https://image.tmdb.org/t/p/original/`;
 export default function Row(props) {
-
   const [movies, setMovies] = useState([]);
 
-  useEffect(()=>{
+  useEffect(() => {
     //[] is used to run the row only once
-    async function fetchData(){
-        const request = await axios.get(props.fetchUrl);
-        console.log(request);
-        return request;
+    async function fetchData() {
+      const request = await axios.get(props.fetchUrl);
+      // console.log(request.data.results);
+      setMovies(request.data.results);
+      return request;
     }
     fetchData();
-  },[])
-  return <div>
+  }, [props.fetchUrl]);
+
+  console.log(movies);
+
+  return (
+    <div className="row">
       <h2>{props.title}</h2>
-  </div>;
+      <div className="row_posters">
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            className="row_poster"
+            src={`${base_url}${movie.poster_path}`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
